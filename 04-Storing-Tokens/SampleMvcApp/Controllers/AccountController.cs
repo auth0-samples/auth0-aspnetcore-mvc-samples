1d03605c8bd2984ec32251a7b2666a573199dac5
Jerrie Pelser <jerrie@jerriepelser.com>
2016-07-11 17:07:06 +0800

    Add tokens sample. Reorganize samples folder

---
+++ 04-Storing-Tokens/SampleMvcApp/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Authentication;
+
+namespace SampleMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = "/" });
+        }
+
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Logout()
+        {
+            HttpContext.Authentication.SignOutAsync("Auth0");
+            HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
