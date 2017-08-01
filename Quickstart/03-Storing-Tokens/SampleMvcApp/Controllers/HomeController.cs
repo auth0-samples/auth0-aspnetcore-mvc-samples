20fcbe85da1a51c585ae46db52124903e5959134
Jerrie Pelser <jerrie@jerriepelser.com>
2017-08-01 15:36:18 -0500

    Update quickstarts to 2.0.0-preview2-final

--- Quickstart/03-Storing-Tokens/SampleMvcApp/Controllers/HomeController.cs
+++ Quickstart/03-Storing-Tokens/SampleMvcApp/Controllers/HomeController.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 
 namespace SampleMvcApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // If the user is authenticated, then this is how you can get the access_token and id_token
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                string idToken = await HttpContext.GetTokenAsync("id_token");
+
+                // Now you can use them. For more info on when and how to use the 
+                // access_token and id_token, see https://auth0.com/docs/tokens
+            }
+
             return View();
         }
 
--- Quickstart/04-User-Profile/SampleMvcApp/Controllers/AccountController.cs
+++ Quickstart/04-User-Profile/SampleMvcApp/Controllers/AccountController.cs
@@ -1,32 +1,34 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using SampleMvcApp.ViewModels;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace SampleMvcApp.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login(string returnUrl = "/")
+        public async Task Login(string returnUrl = "/")
         {
-            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+            await HttpContext.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, 
+                new AuthenticationProperties() { RedirectUri = returnUrl });
         }
 
         [Authorize]
         public async Task Logout()
         {
-            await HttpContext.Authentication.SignOutAsync("Auth0", new AuthenticationProperties
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
             {
                 // Indicate here where Auth0 should redirect the user after a logout.
                 // Note that the resulting absolute Uri must be whitelisted in the 
                 // **Allowed Logout URLs** settings for the client.
                 RedirectUri = Url.Action("Index", "Home")
             });
-            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
 
         [Authorize]
@@ -39,6 +41,7 @@ namespace SampleMvcApp.Controllers
                 ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
             });
         }
+
 
         /// <summary>
         /// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
