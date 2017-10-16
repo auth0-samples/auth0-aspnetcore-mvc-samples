4efce7d11c0bf6ae676d4a790f6b84293e70ce66
Jerrie Pelser <jerrie@jerriepelser.com>
2017-10-16 09:25:20 +0700

    remove custom login

---
+++ Samples/custom-login/SampleMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SampleMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
