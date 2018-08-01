678e610f0fbbae93cf6e4d7086b56301a846b43e
Alexis Luque <aluque@indexlabs.com.uy>
2018-08-01 14:27:48 -0300

    Disable cookie consent bar

--- Quickstart/01-Login/SampleMvcApp/Startup.cs
+++ Quickstart/01-Login/SampleMvcApp/Startup.cs
@@ -29,7 +29,7 @@ namespace SampleMvcApp
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
