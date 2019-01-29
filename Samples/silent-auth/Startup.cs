fa1d2d07fb557493ba3f9a4aa0e3b0135e997f9e
Alexis Luque <aluque@indexlabs.com.uy>
2019-01-29 15:51:31 -0300

    Add missing namespace

--- Samples/silent-auth/Startup.cs
+++ Samples/silent-auth/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
