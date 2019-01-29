263b4e643d85fe27a5ded25581b00457bb2d4fef
Alexis Luque <aluque@indexlabs.com.uy>
2019-01-29 17:33:09 -0300

    Dockerize oauth2 and silent-auth samples

---
+++ Samples/custom-login/exec.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t auth0-aspnetcore-v1-mvc-custom-login .
+docker run -it -p 3000:3000 -e "ASPNETCORE_URLS=http://*:3000" auth0-aspnetcore-v1-mvc-custom-login
---
+++ Samples/silent-auth/exec.ps1
@@ -0,0 +1,2 @@
+docker build -t auth0-aspnetcore-v1-mvc-silent-auth .
+docker run -it -p 3000:3000 -e "ASPNETCORE_URLS=http://*:3000" auth0-aspnetcore-v1-mvc-silent-auth
