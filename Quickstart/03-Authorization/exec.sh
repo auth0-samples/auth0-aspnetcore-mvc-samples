932b6bb3856565d8544875aabf855ca3760d1f15
Alexis Luque <aluque@indexlabs.com.uy>
2019-01-30 13:11:19 -0300

    Fix typos

--- Quickstart/03-Authorization/exec.sh
+++ Quickstart/03-Authorization/exec.sh
@@ -1,2 +1,2 @@
-docker build -t auth0-aspnetcore-v1-mvc-03authorization .
-docker run -p 3000:3000 -e "ASPNETCORE_URLS=http://*:3000" auth0-aspnetcore-v1-mvc-03authorization
+docker build -t auth0-aspnetcore-v1-mvc-03-authorization .
+docker run -p 3000:3000 -e "ASPNETCORE_URLS=http://*:3000" auth0-aspnetcore-v1-mvc-03-authorization
