69390e33828104c8bdbfec13eceb996c73c8cae6
Sean Killeen <SeanKilleen@gmail.com>
2020-01-25 22:13:22 -0500

    Cleanup unused references

--- Quickstart/01-Login/Startup.cs
+++ Quickstart/01-Login/Startup.cs
@@ -6,10 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
