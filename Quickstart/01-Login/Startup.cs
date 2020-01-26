1fd2048684e3b0b7cd20a170cd703f66cd897a9c
Sean Killeen <SeanKilleen@gmail.com>
2020-01-25 22:08:02 -0500

    Update MVC compatibility version

--- Quickstart/01-Login/Startup.cs
+++ Quickstart/01-Login/Startup.cs
@@ -96,7 +96,7 @@ namespace SampleMvcApp
             });
 
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
