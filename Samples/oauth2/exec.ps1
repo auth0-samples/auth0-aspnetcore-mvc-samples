9c4741cc09913db6d5b268b8a9417079ba732166
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-02-11 11:22:24 +0000

    Remove samples and references to those samples

--- Samples/oauth2/exec.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t auth0-aspnetcore-mvc-oauth2 .
-docker run -it -p 3000:3000 auth0-aspnetcore-mvc-oauth2
--- Samples/oidc-hs256/exec.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t auth0-aspnetcore-mvc-oidc-hs256 .
-docker run -it -p 3000:3000 auth0-aspnetcore-mvc-oidc-hs256
